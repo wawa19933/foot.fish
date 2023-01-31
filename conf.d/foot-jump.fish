c5854e54e8d65ea25426f441d4d7d1f8465bd13a
Sergio Scalpone <wawa19933@gmail.com>
2023-01-31 21:38:59 +0100

    add check for terminal

--- conf.d/foot-jump.fish
+++ conf.d/foot-jump.fish
@@ -1,4 +1,5 @@
-function mark_prompt_start --on-event fish_prompt
-    echo -en "\e]133;A\e\\"
+if status is-interactive; and string match "foot*" $TERM >&2 2>/dev/null
+    function mark_prompt_start --on-event fish_prompt
+        echo -en "\e]133;A\e\\"
+    end
 end
-
